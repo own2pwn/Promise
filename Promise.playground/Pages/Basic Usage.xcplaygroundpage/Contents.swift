c375f38f1acdf4dccb82c18309b7f6eb4bbec94a
Evgeniy <7850039+own2pwn@users.noreply.github.com>
2019-03-29 13:38:27 +0300

    playground target mac os

--- Promise.playground/Pages/Basic Usage.xcplaygroundpage/Contents.swift
+++ Promise.playground/Pages/Basic Usage.xcplaygroundpage/Contents.swift
@@ -1,16 +1,9 @@
+import Foundation
+import PlaygroundSupport
+import Promise
 
-import Promise  // <-- If there is an error here, build the project first.
-
-/*
-
- Basic usage of a Promise.
-
- */
-
-/// Helper function to build/fulfill a Promise.
-/// - returns: A Promise that fulfills with the given string, after a delay.
 func promisedString(_ str: String) -> Promise<String> {
-    return Promise<String>(work: { fulfill, reject in
+    return Promise<String>(work: { fulfill, _ in
         print("sleeping…")
         sleep(1)
         print("done sleeping")
@@ -18,19 +11,12 @@ func promisedString(_ str: String) -> Promise<String> {
     })
 }
 
-
 promisedString("simple example").then({ result in
     print("Got result: ", result)
 })
 
 print("after creating promise")
 
-
-
-
-
-
 // - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // Run the playground so the `dispatch_async()`s work
-import XCPlayground
-XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+PlaygroundPage.current.needsIndefiniteExecution = true
